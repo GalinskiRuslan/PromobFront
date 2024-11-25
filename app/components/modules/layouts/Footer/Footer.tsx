4c34ed949124c4d4b5089631a334562ebe6b9060
Ruslan Issapov <r_issapov@almaty.gamma.kz>
2024-11-25 16:05:06 +0500

    Footer

--- app/components/modules/layouts/Footer/Footer.tsx
+++ app/components/modules/layouts/Footer/Footer.tsx
@@ -1,4 +1,8 @@
 import cl from "./style.module.css";
+import Image from "next/image";
+import whatsapp from "./assets/whatsApp.png";
+import telegramm from "./assets/telega.png";
+import insta from "./assets/instagram.png";
 
 type Props = {};
 
@@ -7,15 +11,26 @@ export const Footer = (props: Props) => {
     <div className={cl.container}>
       <div className={cl.innerContainer}>
         <div>
-          <div>
+          <a href="/">
             <button className={cl.proBtn}>PROmobilograf</button>
+          </a>
+        </div>
+        <div>
+          <p className={cl.title}>Наши социальные сети</p>
+          <div className={cl.socialCont}>
+            <a href="https://wa.me/77022638953">
+              <Image src={whatsapp} alt="wp" />
+            </a>
+            <a href="https://t.me/@promobilograf_kz">
+              <Image src={telegramm} alt="telega" />
+            </a>
+            <a href="https://www.instagram.com/promobilograf_kz?igsh=MTNpdGtzcG8yNXpxNQ==">
+              <Image src={insta} alt="inst" />
+            </a>
           </div>
-          <div></div>
-          <div></div>
-          <div></div>
-          <div></div>
         </div>
       </div>
+      <p className={cl.copyriter}>© 2024 Все права защищены</p>
     </div>
   );
 };
